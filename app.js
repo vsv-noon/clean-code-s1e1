5f8067dc33de5ca5bd92ca2d16dd02854f5b5d3f
ViacheslavAnisimov <forvsn@gmail.com>
2024-12-16 18:00:20 +0700

    refactor(extended-1.2): add alternative descriptions for images

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt="delete-button";
     deleteButtonImg.className="btn-delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
